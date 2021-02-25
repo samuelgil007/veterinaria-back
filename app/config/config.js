2bc33bedfcc1ed33740d75e8693fa6da87a4571c
Samuel Gil <53627477+samuelgil007@users.noreply.github.com>
2021-02-24 19:37:10 -0500

    last change

--- app/config/config.js
+++ app/config/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: process.env.MONGODB_URI || 'mongodb://analytics:analytics-password@cluster0-shard-00-00.za8im.mongodb.net:27017,cluster0-shard-00-01.za8im.mongodb.net:27017,cluster0-shard-00-02.za8im.mongodb.net:27017/veterinaria?ssl=true&replicaSet=atlas-ur9ln5-shard-0&authSource=admin&retryWrites=true&w=majority',
+    DB: process.env.MONGODB_URI ,
     SECRET_TOKEN: process.env.SECRET_TOKEN || 'estaeslallave'
 }
